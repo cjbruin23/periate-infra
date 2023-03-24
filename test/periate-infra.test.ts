db45da447ec22f21602ce7412f94013aefa54632
Colin Bruin <colinjb23@gmail.com>
2023-03-24 11:12:35 -0700

    Initial commit

---
+++ test/periate-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PeriateInfra from '../lib/periate-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/periate-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PeriateInfra.PeriateInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
